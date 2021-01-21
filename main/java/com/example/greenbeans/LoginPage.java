37ddd4a1cda13967b569aa79df83561210ab3d9b
RH3038 <46035521+RH3038@users.noreply.github.com>
2021-01-21 15:40:53 -0500

    Add files via upload

---
+++ main/java/com/example/greenbeans/LoginPage.java
@@ -0,0 +1,13 @@
+package com.example.greenbeans;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
---
+++ main/java/com/example/greenbeans/PasswordReset.java
@@ -0,0 +1,13 @@
+package com.example.greenbeans;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PasswordReset extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_reset_page);
+    }
+}
